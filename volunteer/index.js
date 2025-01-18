b4290a7dbda6d3120a2e7f01a2d79f895b462181
sasi1565 <sasikiran1565@gmail.com>
2025-01-18 20:49:26 +0530

    made some modifications

---
+++ volunteer/index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.toggle-edit').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const input = e.target.previousElementSibling;
+        input.readOnly = !input.readOnly;
+        e.target.textContent = input.readOnly ? 'Edit' : 'Lock';
+    });
+});
+
+
